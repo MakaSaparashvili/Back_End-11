2dcbdb39a99ed0f6602f96ede8242a77b0f619fc
maka <maka.saparashvili@gmail.com>
2025-09-30 21:04:27 +0400

    views, urls updated

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogPost
 
-# Create your views here.
+
+def blog_list(request):
+    posts = BlogPost.objects.all().order_by('-created_at')
+    return render(request, 'blog_app/blog_list.html', {'posts': posts})
+
+def blog_detail(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    return render(request, 'blog_app/blog_detail.html', {'post': post})
