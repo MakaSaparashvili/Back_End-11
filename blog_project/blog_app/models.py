fb50c7d5e9afe52d24cb63000a4eff8fd702503d
maka <maka.saparashvili@gmail.com>
2025-09-30 17:52:05 +0400

    Module created

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=200)
+    content = models.TextField(verbose_name="Content")
+    author = models.CharField(verbose_name="Author",max_length=100)
+    created_at = models.DateTimeField(verbose_name="created date",auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+
+    def __str__(self):
+        return self.title
